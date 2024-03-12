b4c95a35b9911f562a53775be4fd8b5cdbc3c14e
omprakash <kanuparewa371@gmail.com>
2024-03-12 12:11:58 +0530

    first commit

---
+++ models/url.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ShortUrlSchema = new Schema({
+    url:{
+        type:String,
+        require:true,
+
+    },
+    shortId:{
+        type:String,
+        require:true,
+
+    }
+
+})
+
+
+const ShortUrlModel  = mongoose.model('shortUrl',ShortUrlSchema)
+
+module.exports = ShortUrlModel; 
+
